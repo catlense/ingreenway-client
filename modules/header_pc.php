69065f33f20a4883bbee5109c3d683a0f81424cd
Maxim Voronin <catlense@voronin.xyz>
2021-08-13 03:26:45 +0700

    Header template

--- modules/header_pc.php
+++ modules/header_pc.php
@@ -9,7 +9,48 @@
             </div>
             <div class="right_links">
                 <a>Присоединиться / Войти</a>
+                <div class="country-selector">
+                    <div class="flag">
+                        <img src="/static/images/regions/flag-ru.png" alt="">
+                    </div>
+                    <div class="country">Россия <i class="mdi mdi-menu-down"></i></div>
+                </div>
             </div>
+        </div>
+        <div class="main-content font-gotham">
+            <a href="/" class="logo">
+                <img src="/static/images/logo.svg" alt="">
+            </a>
+            <div class="right-icons">
+                <div class="search" id="header-search">
+                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="74" height="74" id="icon-search" viewBox="0 0 24 24">
+					    <path d="M17.6125 15.4913C18.7935 13.8785 19.4999 11.8975 19.4999 9.74998C19.4999 4.37402 15.1259 0 9.74993 0C4.37398 0 0 4.37402 0 9.74998C0 15.1259 4.37403 19.5 9.74998 19.5C11.8975 19.5 13.8787 18.7934 15.4915 17.6124L21.8789 23.9999L24 21.8788C24 21.8787 17.6125 15.4913 17.6125 15.4913ZM9.74998 16.4999C6.02781 16.4999 3.00001 13.4721 3.00001 9.74998C3.00001 6.02781 6.02781 3.00001 9.74998 3.00001C13.4721 3.00001 16.4999 6.02781 16.4999 9.74998C16.4999 13.4721 13.4721 16.4999 9.74998 16.4999Z"></path>
+					</svg>
+                </div>
+                <div class="cart">
+                    <svg class="icon icon-menu-cart" xmlns="http://www.w3.org/2000/svg" width="74" height="74" id="icon-menu-cart" viewBox="0 0 24 24">
+                        <path d="M23.9965 22.001L22.7862 1.99896C22.7198 0.901738 21.7676 0.00512123 20.6599 0.00512123H3.3521C2.2354 0.00512123 1.29199 0.897803 1.22477 1.99896L0.0039759 22.001C-0.0629954 23.0983 0.779456 23.9949 1.88617 23.9949H22.1133C23.2258 23.9949 24.0631 23.1022 23.9965 22.001ZM2.71544 21.3293L3.85426 2.67067H20.1565L21.2855 21.3294H2.71544V21.3293Z"></path>
+                        <path d="M14.6588 5.33616V8.70166C14.6588 8.89863 14.5785 9.30021 14.3487 9.68355C13.9668 10.3204 13.2781 10.7034 11.9932 10.7034C10.7083 10.7034 10.0196 10.3205 9.63778 9.68355C9.40794 9.30021 9.32766 8.89863 9.32766 8.70166V5.33616H6.66211V8.70166C6.66211 9.33826 6.83172 10.1871 7.35157 11.0542C8.2192 12.5014 9.77953 13.3689 11.9932 13.3689C14.2068 13.3689 15.7671 12.5014 16.6347 11.0542C17.1546 10.1871 17.3242 9.33826 17.3242 8.70166V5.33616H14.6588Z"></path>
+                    </svg>
+                </div>
+                <div class="partner">
+                    <img src="/static/images/ui-icons/user.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="categories">
+        <div class="container font-gotham">
+            <ul>
+                <li><a href="/shop">Все товары</a></li>
+                <li><a href="/shop/home">Дом</a></li>
+                <li><a href="/shop/beauty">Красота и уход</a></li>
+                <li><a href="/shop/health">Здоровье</a></li>
+                <li><a href="/shop/eat">Питание</a></li>
+                <li><a href="/shop/textile">Текстиль</a></li>
+                <li><a href="/shop/tea">Чаи и напитки</a></li>
+                <li><a href="/brands">Бренды</a></li>
+            </ul>
         </div>
     </div>
 </header>
