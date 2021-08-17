acb3e3cdfbbd38276a525205bfd4df96743c9f6c
Maxim Voronin <catlense@voronin.xyz>
2021-08-18 00:09:35 +0700

    Mobile header

--- js/ui/flash.js
+++ js/ui/flash.js
@@ -2,17 +2,20 @@ let flash = document.querySelector('.flash')
 let flashClose = document.querySelector('.close-flash')
 
 let headerPc = document.querySelector('header.pc')
+let headerMob = document.querySelector('header.mobile')
 
 if (flash && flashClose) {
     flashClose.onclick = () => {
         flash.classList.add('flash_hidden')
         if (headerPc) {
             headerPc.style.marginTop = '0px'
+            headerMob.style.marginTop = '0px'
         }
         fetch('/modules/front.php?method=hide_flash')
     }
 
     if (headerPc) {
         headerPc.style.marginTop = flash.scrollHeight + 'px'
+        headerMob.style.marginTop = flash.scrollHeight + 'px'
     }
 }
