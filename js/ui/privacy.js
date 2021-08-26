df81c3b78769ea1074d9b24c60940b416ff7819f
Maxim Voronin <catlense@voronin.xyz>
2021-08-27 00:39:48 +0700

    Privacy page

---
+++ js/ui/privacy.js
@@ -0,0 +1,17 @@
+let faqOpeners = document.querySelectorAll('.faqOpener')
+let faqTexts = document.querySelectorAll('.faqText')
+
+faqOpeners.forEach(element => {
+    element.style.height = document.querySelector(`p.title[p-id="${element.getAttribute('data-id')}"]`).clientHeight + 21 + 'px'
+    element.onclick = () => {
+        if (document.querySelector('.faqOpener.active') && document.querySelector('.faqOpener.active').getAttribute('data-id') != element.getAttribute('data-id')) {
+            document.querySelector('.faqOpener.active').style.height = '105px'
+            document.querySelector('.faqOpener.active').classList.remove('active')
+        }
+        if (element.classList.toggle('active')) {
+            element.style.height = document.querySelector(`.faqText#${element.getAttribute('data-id')}`).clientHeight + 155 + 'px'
+        } else {
+            element.style.height = document.querySelector(`p.title[p-id="${element.getAttribute('data-id')}"]`).clientHeight + 21 + 'px'
+        }
+    }
+})
