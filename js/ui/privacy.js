b4304bcb142c38842ebc77449985a22bd8d41a9c
Maxim Voronin <catlense@voronin.xyz>
2021-08-27 00:44:46 +0700

    privacy hlep

--- js/ui/privacy.js
+++ js/ui/privacy.js
@@ -2,16 +2,32 @@ let faqOpeners = document.querySelectorAll('.faqOpener')
 let faqTexts = document.querySelectorAll('.faqText')
 
 faqOpeners.forEach(element => {
-    element.style.height = document.querySelector(`p.title[p-id="${element.getAttribute('data-id')}"]`).clientHeight + 21 + 'px'
-    element.onclick = () => {
-        if (document.querySelector('.faqOpener.active') && document.querySelector('.faqOpener.active').getAttribute('data-id') != element.getAttribute('data-id')) {
-            document.querySelector('.faqOpener.active').style.height = '105px'
-            document.querySelector('.faqOpener.active').classList.remove('active')
+
+    if (window.innerWidth < 768) {
+        element.style.height = document.querySelector(`p.title[p-id="${element.getAttribute('data-id')}"]`).clientHeight + 2 + 'px'
+        element.onclick = () => {
+            if (document.querySelector('.faqOpener.active') && document.querySelector('.faqOpener.active').getAttribute('data-id') != element.getAttribute('data-id')) {
+                document.querySelector('.faqOpener.active').style.height = '105px'
+                document.querySelector('.faqOpener.active').classList.remove('active')
+            }
+            if (element.classList.toggle('active')) {
+                element.style.height = document.querySelector(`.faqText#${element.getAttribute('data-id')}`).clientHeight + 155 + 'px'
+            } else {
+                element.style.height = document.querySelector(`p.title[p-id="${element.getAttribute('data-id')}"]`).clientHeight + 2 + 'px'
+            }
         }
-        if (element.classList.toggle('active')) {
-            element.style.height = document.querySelector(`.faqText#${element.getAttribute('data-id')}`).clientHeight + 155 + 'px'
-        } else {
-            element.style.height = document.querySelector(`p.title[p-id="${element.getAttribute('data-id')}"]`).clientHeight + 21 + 'px'
+    } else {
+        element.style.height = document.querySelector(`p.title[p-id="${element.getAttribute('data-id')}"]`).clientHeight + 21 + 'px'
+        element.onclick = () => {
+            if (document.querySelector('.faqOpener.active') && document.querySelector('.faqOpener.active').getAttribute('data-id') != element.getAttribute('data-id')) {
+                document.querySelector('.faqOpener.active').style.height = '105px'
+                document.querySelector('.faqOpener.active').classList.remove('active')
+            }
+            if (element.classList.toggle('active')) {
+                element.style.height = document.querySelector(`.faqText#${element.getAttribute('data-id')}`).clientHeight + 155 + 'px'
+            } else {
+                element.style.height = document.querySelector(`p.title[p-id="${element.getAttribute('data-id')}"]`).clientHeight + 21 + 'px'
+            }
         }
     }
 })
