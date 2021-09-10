8790116001f0e0d0368b35350e189e1064fce7a4
Maxim Voronin <catlense@voronin.xyz>
2021-09-11 00:46:43 +0700

    Page product recommendation block

--- js/ui/product.js
+++ js/ui/product.js
@@ -1,7 +1,7 @@
 let tabsNames = document.querySelectorAll('.tabs__name');
 let tabsContents = document.querySelectorAll('.tabs__content');
 
-let feedbacks = document.querySelector('footer')
+let feedbacks = document.querySelector('.recommendation')
 
 let miniImages = document.querySelectorAll('.mini-images .img')
 
@@ -43,6 +43,43 @@ tabsNames.forEach(element => {
             document.querySelector(`#${element.getAttribute('data-id')}`).classList.add('active');
             feedbacks.style.marginTop = document.querySelector(`#${element.getAttribute('data-id')} .tabs__content`).scrollHeight + 30 + 'px'
 
+        }
+
+    }
+})
+
+new Swiper('.products_slider', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    slidesPerGroup: 1,
+    simulateTouch: true,
+    slidesOffsetBefore: 0,
+    slidesOffsetAfter: 0,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+        clickable: true,
+        currentClass: 'swiper-pagination-current',
+        totalClass: 'swiper-pagination-total'
+    },
+    navigation: {
+        nextEl: ".swiper-button__next",
+        prevEl: ".swiper-button__prev",
+    },
+    breakpoints: {
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+            slidesPerGroup: 1,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+            slidesPerGroup: 1
         }
 
     }
